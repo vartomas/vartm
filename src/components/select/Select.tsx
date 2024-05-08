381a16f27248cc79aece7e2cdd51cac20ab0f904
Tomas <tomasvareika85@gmail.com>
2024-05-08 23:42:39 +0300

    fixes

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ReactElement } from 'react';
+import React, { FC } from 'react';
 import { useSelect } from './useSelect';
 import { SelectOption, SelectProps, SelectPropsMultiple } from './types';
 
@@ -19,6 +19,14 @@ const Select: FC<SelectProps | SelectPropsMultiple> = ({
 }) => {
   const { open, containerRef, setOpen, handleSelect } = useSelect(multiple, onChange);
 
+  const createValue = (selection: SelectOption) => {
+    if (multiple) {
+      return value ? [...value, selection] : [selection];
+    }
+
+    return selection;
+  };
+
   return (
     <div className={containerClassName} ref={containerRef}>
       <div className={inputClassName} onClick={() => setOpen((prev) => !prev)}>
@@ -26,9 +34,9 @@ const Select: FC<SelectProps | SelectPropsMultiple> = ({
           renderInput(value as (SelectOption & SelectOption[]) | null)
         ) : value ? (
           multiple ? (
-            value.map((x) => <span className={tagClassName}>x.label</span>)
+            value.map((x) => <span className={tagClassName}>{x.label}</span>)
           ) : (
-            <span className={tagClassName}>value.label</span>
+            <span className={tagClassName}>{value.label}</span>
           )
         ) : (
           <span style={{ opacity: 0.5 }} className={placeHolderClassName}>
@@ -40,7 +48,7 @@ const Select: FC<SelectProps | SelectPropsMultiple> = ({
         <div className={dropdownClassName}>
           {options.map((option, i) => (
             <div key={`${option.label}-${i}`}>
-              <div className={optionClassName} onClick={() => handleSelect(option)}>
+              <div className={optionClassName} onClick={() => handleSelect(createValue(option))}>
                 {renderOption ? renderOption(option) : option.label}
               </div>
             </div>
